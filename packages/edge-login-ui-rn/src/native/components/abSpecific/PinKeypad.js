d459a4a6f46c92b9079e684c54db304e353d361a
Matthew <matthew@edge.app>
2020-06-22 00:48:52 -0700

    Randomize PIN keypad layout

--- packages/edge-login-ui-rn/src/native/components/abSpecific/PinKeypad.js
+++ packages/edge-login-ui-rn/src/native/components/abSpecific/PinKeypad.js
@@ -14,7 +14,45 @@ export type Props = {
   onChangeText(Object): void
 }
 
-class PinKeypad extends Component<Props> {
+type State = {
+  random: boolean
+}
+
+const buttons = {
+  zero: { num: '0', text: s.strings.keypad_zero },
+  one: { num: '1', text: s.strings.keypad_one },
+  two: { num: '2', text: s.strings.keypad_two },
+  three: { num: '3', text: s.strings.keypad_three },
+  four: { num: '4', text: s.strings.keypad_four },
+  five: { num: '5', text: s.strings.keypad_five },
+  six: { num: '6', text: s.strings.keypad_six },
+  seven: { num: '7', text: s.strings.keypad_seven },
+  eight: { num: '8', text: s.strings.keypad_eight },
+  nine: { num: '9', text: s.strings.keypad_nine }
+}
+
+let randButtons = []
+
+function randomizeNumPad(array) {
+  const keys = Object.keys(array)
+  const randKeys = []
+  while (keys.length > 0) {
+    const i = Math.floor(Math.random() * keys.length)
+    const newObj = buttons[keys[i]]
+    randKeys.push(newObj)
+    keys.splice(i, 1)
+  }
+  return randKeys
+}
+
+class PinKeypad extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      random: false
+    }
+  }
+
   changePin = (value: string) => {
     const { username, pin, onChangeText } = this.props
     if (value === 'back') {
@@ -26,6 +64,11 @@ class PinKeypad extends Component<Props> {
     onChangeText({ username: username, pin: pin ? pin.concat(value) : value })
   }
 
+  componentDidMount() {
+    randButtons = randomizeNumPad(buttons)
+    this.setState({ random: true })
+  }
+
   render() {
     const { wait, style } = this.props
     return (
@@ -34,87 +77,141 @@ class PinKeypad extends Component<Props> {
           <View style={style.keypadRow}>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('1')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[0].num : buttons.one.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_one}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[0].text : buttons.one.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('2')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[1].num : buttons.two.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_two}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[1].text : buttons.two.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('3')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[2].num : buttons.three.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_three}</Text>
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
-          <View style={style.keypadRow}>
-            <TouchableWithoutFeedback
-              style={style.keypadColumn}
-              onPress={() => this.changePin('4')}
-              disabled={wait}
-            >
-              <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_four}</Text>
-              </View>
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback
-              style={style.keypadColumn}
-              onPress={() => this.changePin('5')}
-              disabled={wait}
-            >
-              <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_five}</Text>
-              </View>
-            </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback
-              style={style.keypadColumn}
-              onPress={() => this.changePin('6')}
-              disabled={wait}
-            >
-              <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_six}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[2].text : buttons.three.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
           </View>
           <View style={style.keypadRow}>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('7')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[3].num : buttons.four.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_seven}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[3].text : buttons.four.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('8')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[4].text : buttons.five.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_eight}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[4].text : buttons.five.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('9')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[5].num : buttons.six.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_nine}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[5].text : buttons.six.text}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={style.keypadRow}>
+            <TouchableWithoutFeedback
+              style={style.keypadColumn}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[6].num : buttons.seven.num
+                )
+              }
+              disabled={wait}
+            >
+              <View style={style.keypadColumn}>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[6].text : buttons.seven.text}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              style={style.keypadColumn}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[7].num : buttons.eight.num
+                )
+              }
+              disabled={wait}
+            >
+              <View style={style.keypadColumn}>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[7].text : buttons.eight.text}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              style={style.keypadColumn}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[8].num : buttons.nine.num
+                )
+              }
+              disabled={wait}
+            >
+              <View style={style.keypadColumn}>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[8].text : buttons.nine.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
           </View>
@@ -122,11 +219,17 @@ class PinKeypad extends Component<Props> {
             <View style={style.keypadColumnBlank} />
             <TouchableWithoutFeedback
               style={style.keypadColumn}
-              onPress={() => this.changePin('0')}
+              onPress={() =>
+                this.changePin(
+                  this.state.random ? randButtons[9].num : buttons.zero.num
+                )
+              }
               disabled={wait}
             >
               <View style={style.keypadColumn}>
-                <Text style={style.keypadKeys}>{s.strings.keypad_zero}</Text>
+                <Text style={style.keypadKeys}>
+                  {this.state.random ? randButtons[9].text : buttons.zero.text}
+                </Text>
               </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback
